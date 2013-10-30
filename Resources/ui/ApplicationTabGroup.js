f7d24a6b87dcd200e65291a269f7119846d48f4d
Nathan <nteetor@oberlin.edu>
2013-10-30 11:43:24 -0400

    Updated TasksWindow.js, the tasks tab now looks more like it should

--- Resources/ui/ApplicationTabGroup.js
+++ Resources/ui/ApplicationTabGroup.js
@@ -1,35 +1,44 @@
+var DEBUG = function(s) {
+	if (true) {
+		Ti.API.info('DEBUG: ' + s);	// set this to false to turn off debug output
+	}
+};
+
 var TasksWindow = require('ui/TasksWindow');
 var CalendarWindow = require('ui/CalendarWindow');
 
 function ApplicationTabGroup(Window) {
+	// set the global focus date to the current date
+	Ti.App.Properties.setObject('focus_date', new Date());
+	DEBUG('focus date is ' + Ti.App.Properties.getObject('focus_date'));
+
 	//create module instance
 	var self = Ti.UI.createTabGroup();
-	
+
 	var task_tab = Ti.UI.createTab({
-		title: L('tasks'),
-		//icon: '/images/'		
-	});
-		
-	var cal_tab = Ti.UI.createTab({
-		title: L('calendar'),
+		title : L('tasks'),
 		//icon: '/images/'
 	});
-		
+
+	var cal_tab = Ti.UI.createTab({
+		title : L('calendar'),
+		//icon: '/images/'
+	});
+
 	//create app tabs
-	var task_win = new TasksWindow(),
-		cal_win = new CalendarWindow();
-	
+	var task_win = new TasksWindow(), cal_win = new CalendarWindow();
+
 	// I believe we'll use this to point back i.e. for the back button
 	task_win.containingTab = task_tab;
 	cal_win.containingTab = cal_win;
-	
+
 	// Set windows for each tab
 	task_tab.window = task_win;
 	cal_tab.window = cal_win;
-		
+
 	self.addTab(task_tab);
 	self.addTab(cal_tab);
-	
+
 	return self;
 };
 
--- Resources/ui/TasksWindow.js
+++ Resources/ui/TasksWindow.js
@@ -1,9 +1,95 @@
+var db = require('lib/db');
+
+var DEBUG = function(s) {
+	if (true) {
+		Ti.API.info('DEBUG: ' + s);	// set this to false to turn off debug output
+	}
+};
+
+var dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 
+				 'July', 'August', 'September', 'October', 'November', 'December'];
 
 function TasksWindow(){
 	var self = Ti.UI.createWindow({
 		title : L('tasks'),
-		backgroundColor : 'white'
+		backgroundColor : 'white',
+		layout : 'vertical'
 	});
+	
+	var prettyDate = function(date){
+		date = new Date(date);
+		return dayOfWeek[date.getDay()]+', '+monthName[date.getMonth()]+' '+date.getDate()+', '+date.getFullYear();
+	};
+	
+	var self_title = Ti.UI.createLabel({
+		text : prettyDate(Ti.App.Properties.getObject('focus_date')),
+		top : 5,
+		//height : 20
+	});
+	self.add(self_title);
+	
+	self.addEventListener('swipe',function(e){
+		// TODO: we need to consider when we decrement or increment into another month
+		// THIS IS DONE AUTOMATICALLY, HELL YEAH
+		
+		// if swipe left then increment the date
+		if (e.direction == 'left'){
+			var old_focus = new Date(Ti.App.Properties.getObject('focus_date'));
+			var new_focus = old_focus.setDate(old_focus.getDate()+1); // increment date
+			Ti.App.Properties.setObject('focus_date',new_focus);
+			self_title.setText(prettyDate(new_focus));
+			
+		} // if swipe right then decrement date 
+		else if (e.direction == 'right'){
+			var old_focus = new Date(Ti.App.Properties.getObject('focus_date'));
+			var new_focus = old_focus.setDate(old_focus.getDate()-1); // decrement date
+			Ti.App.Properties.setObject('focus_date',new_focus);
+			self_title.setText(prettyDate(new_focus));
+			
+		}
+		
+	});
+	
+	// the table view that will hold the tasks
+	tasks_list = db.daylist(new Date(Ti.App.Properties.getObject('focus_date')));
+	var tasks_table = Ti.UI.createTableView({
+		data : tasks_list,
+		top : 5
+	});
+	
+	// add the table to our window
+	self.add(tasks_table);
+	
+	/*
+	 * The set of buttons for the task window toolbar
+	 */
+	var add = Ti.UI.createButton({
+		systemButton : Titanium.UI.iPhone.SystemButton.ADD
+	});
+	
+	var edit = Ti.UI.createButton({
+		systemButton : Titanium.UI.iPhone.SystemButton.EDIT,
+		enabled : false
+	});
+	
+	var del = Ti.UI.createButton({
+		systemButton : Titanium.UI.iPhone.SystemButton.TRASH,
+		enabled : false
+	});
+	
+	var done = Ti.UI.createButton({
+		systemButton : Titanium.UI.iPhone.SystemButton.DONE,
+	});
+	
+	// used to evenly distribute items on the toolbar
+	var flexSpace = Titanium.UI.createButton({
+		systemButton:Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE
+	});
+
+	// set the toolbar for our window using the above buttons	
+	self.setToolbar([add,flexSpace,edit,flexSpace,del,flexSpace,done]);
 	
 	return self;	
 }
