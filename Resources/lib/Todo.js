a6edb812192c2265a0109d09dab2314deda3fd08
jackrr <jackratner@gmail.com>
2013-11-04 20:51:22 -0500

    remove info line that nate couldn't search to find

--- Resources/lib/Todo.js
+++ Resources/lib/Todo.js
@@ -3,7 +3,6 @@
  * @param {Object} params is an abstract object from which only necessary fields will be extracted
  */
 function Todo(params) {
-	Ti.API.info('value of params.start is '+params.start);
 	return {
 		'start' : new Date(+params.start),
 		'end' : new Date(+params.end),
